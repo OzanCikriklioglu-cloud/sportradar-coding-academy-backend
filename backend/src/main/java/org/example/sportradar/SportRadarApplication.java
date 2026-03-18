ad325ff0124778632533b0f4c591f5a9f5d18401
Ozan Cikriklioglu <ozancikriklioglu@outlook.com>
2026-03-18 22:06:54 +0100

    chore: initial project structure and raw data source

---
+++ backend/src/main/java/org/example/sportradar/SportRadarApplication.java
@@ -0,0 +1,13 @@
+package org.example.sportradar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportRadarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportRadarApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/org/example/sportradar/SportRadarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sportradar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportRadarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
